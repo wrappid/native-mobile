aa6857cb5c8f5ede67c5662b118287319f324faf
sumanta-m <sumanta.m@vixplor.com>
2023-04-05 21:37:54 +0530

    package structure basicc added with example

---
+++ examples/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import {SCTypography} from './src/package';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <SCTypography>iadiashdiash</SCTypography>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
