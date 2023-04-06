066a4d5d3132cf64af86219d684570fe4186bb76
sumanta-m <sumanta.m@vixplor.com>
2023-04-06 22:13:02 +0530

    sc components added

--- examples/App.js
+++ examples/App.js
@@ -8,12 +8,17 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {SCTypography} from './src/package';
+import {SCAvatar} from './src/package';
+import {SCBadge} from './src/package';
 
 function App() {
   return (
     <SafeAreaView>
+      {/* <Text>asdasd</Text> */}
       <View>
         <SCTypography>iadiashdiash</SCTypography>
+        <SCAvatar />
+        <SCBadge>2</SCBadge>
       </View>
     </SafeAreaView>
   );
