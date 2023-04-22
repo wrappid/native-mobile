ca171869af898038b1ea115fcbb26aa67ae1f96c
sumanta-m <sumanta.m@vixplor.com>
2023-04-22 14:58:48 +0530

    native routing

--- package/index.js
+++ package/index.js
@@ -1,87 +1,93 @@
 /**
  * NATIVE COMPONENTS
  */
-import NativeFormContainer from './nativeComponents/forms/NativeFormContainer'
+import NativeFormContainer from "./nativeComponents/forms/NativeFormContainer";
+import {
+  NativeDomRoutes,
+  NativeDomRoute,
+  NativeDomNavigate,
+} from "./nativeComponents/helper/routerHelper";
+import NativeNavigation from "./nativeComponents/navigation/NativeNavigation";
 
 /**
  * SC COMPONENTS
  */
 
-import {SCAvatar} from './dataDisplay/SCAvatar';
-import {SCBadge} from './dataDisplay/SCBadge';
-import {SCChip} from './dataDisplay/SCChip';
-import {SCDivider} from './dataDisplay/SCDivider';
-import {SCIcon} from './dataDisplay/SCIcon';
-import {SCImage} from './dataDisplay/SCImage';
-import {SCListItemIcon} from './dataDisplay/SCListItemIcon';
-import {SCListItemText} from './dataDisplay/SCListItemText';
-import {SCTable} from './dataDisplay/SCTable';
-import {SCTableBody} from './dataDisplay/SCTableBody';
-import {SCTableCell} from './dataDisplay/SCTableCell';
-import {SCTableContainer} from './dataDisplay/SCTableContainer';
-import {SCTableFooter} from './dataDisplay/SCTableFooter';
-import {SCTableHead} from './dataDisplay/SCTableHead';
-import {SCTablePagination} from './dataDisplay/SCTablePagination';
-import {SCTableRow} from './dataDisplay/SCTableRow';
-import {SCTableSortLabel} from './dataDisplay/SCTableSortLabel';
-import {SCTooltip} from './dataDisplay/SCTooltip';
-import {SCTypography} from './dataDisplay/SCTypography';
-import {SCAlert} from './feedback/SCAlert';
-import {SCBackdrop} from './feedback/SCBackdrop';
-import {SCCircularProgress} from './feedback/SCCircularProgress';
-import {SCLinearProgress} from './feedback/SCLinearProgress';
-import {SCSkeleton} from './feedback/SCSkeleton';
-import {SCSnackbar} from './feedback/SCSnackbar';
-import {SCFormGroup} from './form/SCFormGroup';
-import {SCAutocomplete} from './inputs/SCAutocomplete';
-import {SCButton} from './inputs/SCButton';
-import {SCCheckbox} from './inputs/SCCheckbox';
-import {SCDatePicker} from './inputs/SCDatePicker';
-import {SCDateTimePicker} from './inputs/SCDateTimePicker';
-import {SCFab} from './inputs/SCFab';
-import {SCFormControl} from './inputs/SCFormControl';
-import {SCFormControlLabel} from './inputs/SCFormControlLabel';
-import {SCFormHelperText} from './inputs/SCFormHelperText';
-import {SCIconButton} from './inputs/SCIconButton';
-import {SCInput} from './inputs/SCInput';
-import {SCInputAdornment} from './inputs/SCInputAdornment';
-import {SCInputLabel} from './inputs/SCInputLabel';
-import {SCListItemButton} from './inputs/SCListItemButton';
-import {SCSelect} from './inputs/SCSelect';
-import {SCSwitch} from './inputs/SCSwitch';
-import {SCTextField} from './inputs/SCTextField';
-import {SCTimePicker} from './inputs/SCTimePicker';
-import {SCRichTextEditor} from './inputs/custom/SCRichTextEditor';
-import {SCAppDiv} from './layouts/SCAppDiv';
-import {SCBox} from './layouts/SCBox';
-import {SCContainer} from './layouts/SCContainer';
-import {SCGrid} from './layouts/SCGrid';
-import {SCGridItem} from './layouts/SCGridItem';
-import {SCList} from './layouts/SCList';
-import {SCListItem} from './layouts/SCListItem';
-import {SCStack} from './layouts/SCStack';
-import {SCDrawer} from './navigation/SCDrawer';
-import {SCLink} from './navigation/SCLink';
-import {SCMenuItem} from './navigation/SCMenuItem';
-import {SCMenuList} from './navigation/SCMenuList';
-import {SCRightDrawer} from './navigation/SCRightDrawer';
-import {SCTab} from './navigation/SCTab';
-import {SCTabs} from './navigation/SCTabs';
-import {SCAccordion} from './surfaces/SCAccordion';
-import {SCAccordionDetail} from './surfaces/SCAccordionDetail';
-import {SCAccordionSummery} from './surfaces/SCAccordionSummery';
-import {SCAppBar} from './surfaces/SCAppBar';
-import {SCCard} from './surfaces/SCCard';
-import {SCCardActionArea} from './surfaces/SCCardActionArea';
-import {SCCardActions} from './surfaces/SCCardActions';
-import {SCCardContent} from './surfaces/SCCardContent';
-import {SCCardHeader} from './surfaces/SCCardHeader';
-import {SCCardMedia} from './surfaces/SCCardMedia';
-import {SCPaper} from './surfaces/SCPaper';
-import {SCToolbar} from './surfaces/SCToolbar';
-import {SCFade} from './utils/SCFade';
-import {SCModal} from './utils/SCModal';
-import {SCPopover} from './utils/SCPopover';
+import { SCAvatar } from "./dataDisplay/SCAvatar";
+import { SCBadge } from "./dataDisplay/SCBadge";
+import { SCChip } from "./dataDisplay/SCChip";
+import { SCDivider } from "./dataDisplay/SCDivider";
+import { SCIcon } from "./dataDisplay/SCIcon";
+import { SCImage } from "./dataDisplay/SCImage";
+import { SCListItemIcon } from "./dataDisplay/SCListItemIcon";
+import { SCListItemText } from "./dataDisplay/SCListItemText";
+import { SCTable } from "./dataDisplay/SCTable";
+import { SCTableBody } from "./dataDisplay/SCTableBody";
+import { SCTableCell } from "./dataDisplay/SCTableCell";
+import { SCTableContainer } from "./dataDisplay/SCTableContainer";
+import { SCTableFooter } from "./dataDisplay/SCTableFooter";
+import { SCTableHead } from "./dataDisplay/SCTableHead";
+import { SCTablePagination } from "./dataDisplay/SCTablePagination";
+import { SCTableRow } from "./dataDisplay/SCTableRow";
+import { SCTableSortLabel } from "./dataDisplay/SCTableSortLabel";
+import { SCTooltip } from "./dataDisplay/SCTooltip";
+import { SCTypography } from "./dataDisplay/SCTypography";
+import { SCAlert } from "./feedback/SCAlert";
+import { SCBackdrop } from "./feedback/SCBackdrop";
+import { SCCircularProgress } from "./feedback/SCCircularProgress";
+import { SCLinearProgress } from "./feedback/SCLinearProgress";
+import { SCSkeleton } from "./feedback/SCSkeleton";
+import { SCSnackbar } from "./feedback/SCSnackbar";
+import { SCFormGroup } from "./form/SCFormGroup";
+import { SCAutocomplete } from "./inputs/SCAutocomplete";
+import { SCButton } from "./inputs/SCButton";
+import { SCCheckbox } from "./inputs/SCCheckbox";
+import { SCDatePicker } from "./inputs/SCDatePicker";
+import { SCDateTimePicker } from "./inputs/SCDateTimePicker";
+import { SCFab } from "./inputs/SCFab";
+import { SCFormControl } from "./inputs/SCFormControl";
+import { SCFormControlLabel } from "./inputs/SCFormControlLabel";
+import { SCFormHelperText } from "./inputs/SCFormHelperText";
+import { SCIconButton } from "./inputs/SCIconButton";
+import { SCInput } from "./inputs/SCInput";
+import { SCInputAdornment } from "./inputs/SCInputAdornment";
+import { SCInputLabel } from "./inputs/SCInputLabel";
+import { SCListItemButton } from "./inputs/SCListItemButton";
+import { SCSelect } from "./inputs/SCSelect";
+import { SCSwitch } from "./inputs/SCSwitch";
+import { SCTextField } from "./inputs/SCTextField";
+import { SCTimePicker } from "./inputs/SCTimePicker";
+import { SCRichTextEditor } from "./inputs/custom/SCRichTextEditor";
+import { SCAppDiv } from "./layouts/SCAppDiv";
+import { SCBox } from "./layouts/SCBox";
+import { SCContainer } from "./layouts/SCContainer";
+import { SCGrid } from "./layouts/SCGrid";
+import { SCGridItem } from "./layouts/SCGridItem";
+import { SCList } from "./layouts/SCList";
+import { SCListItem } from "./layouts/SCListItem";
+import { SCStack } from "./layouts/SCStack";
+import { SCDrawer } from "./navigation/SCDrawer";
+import { SCLink } from "./navigation/SCLink";
+import { SCMenuItem } from "./navigation/SCMenuItem";
+import { SCMenuList } from "./navigation/SCMenuList";
+import { SCRightDrawer } from "./navigation/SCRightDrawer";
+import { SCTab } from "./navigation/SCTab";
+import { SCTabs } from "./navigation/SCTabs";
+import { SCAccordion } from "./surfaces/SCAccordion";
+import { SCAccordionDetail } from "./surfaces/SCAccordionDetail";
+import { SCAccordionSummery } from "./surfaces/SCAccordionSummery";
+import { SCAppBar } from "./surfaces/SCAppBar";
+import { SCCard } from "./surfaces/SCCard";
+import { SCCardActionArea } from "./surfaces/SCCardActionArea";
+import { SCCardActions } from "./surfaces/SCCardActions";
+import { SCCardContent } from "./surfaces/SCCardContent";
+import { SCCardHeader } from "./surfaces/SCCardHeader";
+import { SCCardMedia } from "./surfaces/SCCardMedia";
+import { SCPaper } from "./surfaces/SCPaper";
+import { SCToolbar } from "./surfaces/SCToolbar";
+import { SCFade } from "./utils/SCFade";
+import { SCModal } from "./utils/SCModal";
+import { SCPopover } from "./utils/SCPopover";
 
 export {
   SCAvatar,
@@ -163,6 +169,9 @@ export {
   /**
    * NATIVE EXPORTS
    */
-
   NativeFormContainer,
+  NativeDomRoutes,
+  NativeDomRoute,
+  NativeDomNavigate,
+  NativeNavigation,
 };
---
+++ package/nativeComponents/helper/routerHelper.js
@@ -0,0 +1,7 @@
+import {
+  Routes as NativeDomRoutes,
+  Route as NativeDomRoute,
+  Navigate as NativeDomNavigate,
+} from "react-router-native";
+
+export { NativeDomRoutes, NativeDomRoute, NativeDomNavigate };
