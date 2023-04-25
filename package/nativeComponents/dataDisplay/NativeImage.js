7c410f2d5c7f4871f0f6847e471adec7e64f486a
sumanta-m <sumanta.m@vixplor.com>
2023-04-25 10:47:53 +0530

    native image coorection

--- package/nativeComponents/dataDisplay/NativeImage.js
+++ package/nativeComponents/dataDisplay/NativeImage.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import {SCImage} from '../../dataDisplay/SCImage';
+import React from "react";
+import { SCImage } from "../../dataDisplay/SCImage";
 
 export default function NativeImage(props) {
-  const {src, ...restProps} = props;
+  const { src, height, width, ...restProps } = props;
 
-  return <SCImage source={{uri: src}} {...restProps} />;
+  return (
+    <SCImage
+      style={{ height, width }}
+      source={
+        src
+          ? typeof src === "string" && src.includes("http")
+            ? { uri: src }
+            : src
+          : ""
+      }
+      {...restProps}
+    />
+  );
 }
