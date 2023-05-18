afdb8e20e456c3982b1727b35db555b5e4a17bff
sumanta-m <sumanta.m@vixplor.com>
2023-05-18 22:44:17 +0530

    pop over made with custom

--- package/nativeComponents/utils/NativePopover.js
+++ package/nativeComponents/utils/NativePopover.js
@@ -1,21 +1,52 @@
-import React from "react";
-import { SCPopover } from "../../styledComponents/utils/SCPopover";
+import React from 'react';
+import NativeBox from '../layouts/NativeBox';
+import NativeIcon from '../dataDisplay/NativeIcon';
+import {UtilityClasses} from '@wrappid/styles';
+import {NativeIconButton} from '@wrappid/styled-components';
 
 export default function NativePopover(props) {
-  const { open, onClose, ...restProps } = props;
+  const {open, onClose, ...restProps} = props;
 
-  console.log("open", open);
-  console.log("restPorps", restProps);
+  console.log('open', open);
+  console.log('restPorps', restProps);
 
   return (
-    <SCPopover
-      visible={open}
-      onDismiss={() => {
-        onClose && onClose();
-      }}
-      {...restProps}
-    >
-      {restProps.children}
-    </SCPopover>
+    props.open && (
+      <NativeBox
+        styleClasses={[
+          UtilityClasses?.DISPLAY?.FLEX,
+          UtilityClasses?.POSITION?.FIXED_TOP,
+          UtilityClasses?.POSITION?.FIXED_BOTTOM,
+        ]}
+        // @todo should be removed if background color opacity support
+        // can be given in styleclasses
+        style={{
+          backgroundColor: 'rgba(0,0,0,0.3)',
+        }}>
+        <NativeBox
+          style={{marginTop: 320, marginBottom: 300, marginLeft: 50}}
+          styleClasses={[
+            UtilityClasses?.DISPLAY?.FLEX,
+            UtilityClasses?.OPACITY?.OPACITY_100,
+            UtilityClasses?.WIDTH?.W_75,
+          ]}>
+          <NativeBox
+            styleClasses={[UtilityClasses?.ALIGNMENT?.ALIGN_ITEMS_END]}>
+            <NativeIconButton onClick={() => onClose && onClose()}>
+              <NativeIcon name="close" type="material-icon">
+                close
+              </NativeIcon>
+            </NativeIconButton>
+          </NativeBox>
+          <NativeBox
+            styleClasses={[
+              UtilityClasses?.BG?.BG_WHITE,
+              UtilityClasses?.PADDING?.P3,
+            ]}>
+            {props.children}
+          </NativeBox>
+        </NativeBox>
+      </NativeBox>
+    )
   );
 }
