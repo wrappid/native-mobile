cc3a890c46062a4620eacc18b06c18bc10c55bc5
sumanta-m <sumanta.m@vixplor.com>
2023-06-17 18:35:40 +0530

    right drawer fix

--- package/nativeComponents/surfaces/NativeRightDrawer.js
+++ package/nativeComponents/surfaces/NativeRightDrawer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { SCRightDrawer } from "../../styledComponents/navigation/SCRightDrawer";
+import { SCRightDrawer } from "../../styledComponents/navigation/SCRightDrawer";
+import NativeFullModal from "../utils/NativeFullModal";
 
 export default function NativeRightDrawer(props) {
   const rightMenuOpen = useSelector((state) => state?.menu?.rightMenuOpen);
   return (
-    <SCRightDrawer open={rightMenuOpen} {...props}>
-      {props.children}
-    </SCRightDrawer>
+    <NativeFullModal
+      open={rightMenuOpen}
+      onOpen={props.onOpen}
+      onClose={props.onClose}
+      searchBox={false}
+    >
+      <SCRightDrawer open={rightMenuOpen} {...props}>
+        {props.children}
+      </SCRightDrawer>
+    </NativeFullModal>
   );
 }
