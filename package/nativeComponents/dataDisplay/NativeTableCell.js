e86fd5f62e8611c2408ed17f688a835b3ccb3722
sumanta-m <sumanta.m@vixplor.com>
2023-06-28 13:24:41 +0530

    native table related fix

--- package/nativeComponents/dataDisplay/NativeTableCell.js
+++ package/nativeComponents/dataDisplay/NativeTableCell.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { SCTableCell } from "../../styledComponents/dataDisplay/SCTableCell";
 import { SCBox } from "../../styledComponents/layouts/SCBox";
+import { UtilityClasses } from "@wrappid/styles";
 
 export default function NativeTableCell(props) {
-  const { children, ...restProps } = props;
+  const { children, styleClasses, ...restProps } = props;
+
   return (
     <>
       {typeof children === "string" ? (
         <SCTableCell {...restProps}>{children}</SCTableCell>
       ) : (
-        <SCBox {...restProps}>
+        <SCBox
+          {...restProps}
+          styleClasses={[...(styleClasses || []), UtilityClasses?.WIDTH?.W_100]}
+        >
           {children}
         </SCBox>
       )}
--- package/nativeComponents/dataDisplay/NativeTableHeadRow.js
+++ package/nativeComponents/dataDisplay/NativeTableHeadRow.js
@@ -1,6 +1,5 @@
 import React from "react";
-import NativeTableRow from "./NativeTableRow";
 
 export default function NativeTableHeadRow(props) {
-  return <NativeTableRow {...props}>{props.children}</NativeTableRow>;
+  return null;
 }
