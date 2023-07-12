5d518884a8355454555cd291031e30b58fbbf93b
techoneel <contact.techoneel@gmail.com>
2023-07-12 11:12:32 +0530

    + native mobile error text resolved

--- package/nativeComponents/inputs/NativeFormHelperText.js
+++ package/nativeComponents/inputs/NativeFormHelperText.js
@@ -3,9 +3,10 @@ import { useSelector } from "react-redux";
 import { SCFormHelperText } from "../../styledComponents/inputs/SCFormHelperText";
 
 export default function NativeFormHelperText(props) {
+  const { error = false } = props;
   const { showHelperText = true } = useSelector((state) => state.forms);
   return (
-    <SCFormHelperText {...props}>
+    <SCFormHelperText type={error ? "error" : ""} {...props}>
       {showHelperText && props.children}
     </SCFormHelperText>
   );
