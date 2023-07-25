8f4bfea9036149d8eaf11a0809a5511d49a49f52
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-07-25 12:38:39 +0530

    + Dialogbox rendering behind Modal issue fixed

--- package/nativeComponents/feedback/NativeDialog.js
+++ package/nativeComponents/feedback/NativeDialog.js
@@ -9,6 +9,7 @@ import NativeTextButton from "../inputs/NativeTextButton";
 import { UtilityClasses } from "@wrappid/styles";
 import NativeBox from "../layouts/NativeBox";
 import NativeIcon from "../dataDisplay/NativeIcon";
+import { Modal as RNModal } from "react-native";
 
 export default function NativeDialog(props) {
   const { dialogInitValue, DialogContext } = props;
@@ -16,87 +17,89 @@ export default function NativeDialog(props) {
   console.log("dialog:", dialog, "setDialog:", setDialog);
   return (
     <Portal>
-      <SCDialog
-        visible={dialog?.showDialog || false}
-        onDismiss={() => {
-          setDialog(dialogInitValue);
-        }}
-      >
-        {dialog?.type === "info" ? (
-          <NativeBox
-            style={{ alignItems: "center" }}
-            styleClasses={[UtilityClasses.COLOR.TEXT_ERROR]}
-          >
-            <NativeIcon
-              type="material-icons"
-              size="medium"
-              childrenFlag={true}
-              name="info"
-              styleClasses={[UtilityClasses.COLOR.TEXT_WARNING]}
+      <RNModal visible={dialog?.showDialog || false} transparent={true}>
+        <SCDialog
+          visible={dialog?.showDialog || false}
+          onDismiss={() => {
+            setDialog(dialogInitValue);
+          }}
+        >
+          {dialog?.type === "info" ? (
+            <NativeBox
+              style={{ alignItems: "center" }}
+              styleClasses={[UtilityClasses.COLOR.TEXT_ERROR]}
+            >
+              <NativeIcon
+                type="material-icons"
+                size="medium"
+                childrenFlag={true}
+                name="info"
+                styleClasses={[UtilityClasses.COLOR.TEXT_WARNING]}
+              />
+            </NativeBox>
+          ) : dialog?.type === "error" ? (
+            <NativeBox
+              styleClasses={[
+                UtilityClasses?.ALIGNMENT?.JUSTIFY_CONTENT_CENTER,
+                UtilityClasses.COLOR.TEXT_ERROR,
+              ]}
+            >
+              <NativeIcon
+                type="material-icons"
+                size="medium"
+                childrenFlag={true}
+                name="cancel"
+              />
+            </NativeBox>
+          ) : (
+            <NativeBox
+              styleClasses={[
+                UtilityClasses?.ALIGNMENT?.JUSTIFY_CONTENT_CENTER,
+                UtilityClasses?.COLOR?.TEXT_SUCCESS,
+              ]}
+            >
+              <NativeIcon
+                type="material-icons"
+                size="medium"
+                childrenFlag={true}
+                name="check-circle"
+              />
+            </NativeBox>
+          )}
+          <SCDialogTitle styleClasses={[UtilityClasses.TEXT.TEXT_CENTER]}>
+            {dialog?.title || ""}
+          </SCDialogTitle>
+          <SCDialogContent>
+            <SCDialogContentText>{dialog?.subtitle || ""}</SCDialogContentText>
+          </SCDialogContent>
+          <SCDialogActions>
+            <NativeTextButton
+              label={dialog?.cancelButtonLabel || "Cancel"}
+              OnClick={() => {
+                if (
+                  dialog.cancelButton &&
+                  typeof dialog.cancelButton === "function"
+                ) {
+                  dialog.cancelButton();
+                }
+                setDialog(dialogInitValue);
+              }}
             />
-          </NativeBox>
-        ) : dialog?.type === "error" ? (
-          <NativeBox
-            styleClasses={[
-              UtilityClasses?.ALIGNMENT?.JUSTIFY_CONTENT_CENTER,
-              UtilityClasses.COLOR.TEXT_ERROR,
-            ]}
-          >
-            <NativeIcon
-              type="material-icons"
-              size="medium"
-              childrenFlag={true}
-              name="cancel"
+            <NativeTextButton
+              label={dialog?.doneButtonLabel || "Done"}
+              OnClick={() => {
+                if (
+                  dialog.doneButton &&
+                  typeof dialog.doneButton === "function"
+                ) {
+                  dialog.doneButton();
+                }
+                setDialog(dialogInitValue);
+              }}
             />
-          </NativeBox>
-        ) : (
-          <NativeBox
-            styleClasses={[
-              UtilityClasses?.ALIGNMENT?.JUSTIFY_CONTENT_CENTER,
-              UtilityClasses?.COLOR?.TEXT_SUCCESS,
-            ]}
-          >
-            <NativeIcon
-              type="material-icons"
-              size="medium"
-              childrenFlag={true}
-              name="check-circle"
-            />
-          </NativeBox>
-        )}
-        <SCDialogTitle styleClasses={[UtilityClasses.TEXT.TEXT_CENTER]}>
-          {dialog?.title || ""}
-        </SCDialogTitle>
-        <SCDialogContent>
-          <SCDialogContentText>{dialog?.subtitle || ""}</SCDialogContentText>
-        </SCDialogContent>
-        <SCDialogActions>
-          <NativeTextButton
-            label={dialog?.cancelButtonLabel || "Cancel"}
-            OnClick={() => {
-              if (
-                dialog.cancelButton &&
-                typeof dialog.cancelButton === "function"
-              ) {
-                dialog.cancelButton();
-              }
-              setDialog(dialogInitValue);
-            }}
-          />
-          <NativeTextButton
-            label={dialog?.doneButtonLabel || "Done"}
-            OnClick={() => {
-              if (
-                dialog.doneButton &&
-                typeof dialog.doneButton === "function"
-              ) {
-                dialog.doneButton();
-              }
-              setDialog(dialogInitValue);
-            }}
-          />
-        </SCDialogActions>
-      </SCDialog>
+          </SCDialogActions>
+        </SCDialog>
+      </RNModal>
     </Portal>
   );
 }
