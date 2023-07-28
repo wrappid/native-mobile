3521ab80d05a58a3a7e27b05766a15566323627a
sumanta-m <sumanta.m@vixplor.com>
2023-07-28 12:20:40 +0530

    grid padding added

--- package/nativeComponents/layouts/NativeGrid.js
+++ package/nativeComponents/layouts/NativeGrid.js
@@ -1,27 +1,29 @@
-import React, { Children } from "react";
-import { SCGrid } from "../../styledComponents/layouts/SCGrid";
-import { SCGridItem } from "../../styledComponents/layouts/SCGridItem";
-import { getUUID } from "../../helper/appUtils";
-import { getGridSizeProps } from "../../helper/componentUtil";
-import { Dimensions } from "react-native";
+import React, {Children} from 'react';
+import {SCGrid} from '../../styledComponents/layouts/SCGrid';
+import {SCGridItem} from '../../styledComponents/layouts/SCGridItem';
+import {getUUID} from '../../helper/appUtils';
+import {getGridSizeProps} from '../../helper/componentUtil';
+import {Dimensions} from 'react-native';
 import {
   LARGE_WINDOW_WIDTH,
   MEDIUM_WINDOW_WIDTH,
   SMALL_WINDOW_WIDTH,
   X_LARGE_WINDOW_WIDTH,
-} from "../../helper/constants";
+} from '../../helper/constants';
 
 export default function NativeGrid(props) {
+  const {spacing} = props;
+  let padding = spacing !== null && spacing !== undefined ? spacing * 4 : 4;
   let _uuid = null;
   React.useEffect(() => {
     _uuid = getUUID();
   }, []);
 
-  var containerId = props?.coreId ? "gc_" + props.coreId : "gc_" + _uuid;
+  var containerId = props?.coreId ? 'gc_' + props.coreId : 'gc_' + _uuid;
 
-  let windowWidth = Dimensions.get("window").width;
+  let windowWidth = Dimensions.get('window').width;
 
-  const gridToWidth = (gridProps) => {
+  const gridToWidth = gridProps => {
     const sizeProps = getGridSizeProps(gridProps);
     let gridSize = sizeProps.xs;
     if (windowWidth >= SMALL_WINDOW_WIDTH) {
@@ -37,11 +39,11 @@ export default function NativeGrid(props) {
       gridSize = sizeProps.xl;
     }
     let width_percent = gridSizeToPercentage(gridSize);
-    return { width: width_percent };
+    return {width: width_percent};
   };
 
   const gridSizeToPercentage = (gridSize = 12) => {
-    return (100 / 12) * gridSize + "%";
+    return (100 / 12) * gridSize + '%';
   };
 
   return (
@@ -49,20 +51,18 @@ export default function NativeGrid(props) {
       key={containerId}
       container={props?.container || true}
       item={props?.item || false}
-      style={{ flexDirection: "row", flexWrap: "wrap" }}
-      styleClasses={props?.styleClasses || []}
-    >
+      style={{flexDirection: 'row', flexWrap: 'wrap'}}
+      styleClasses={props?.styleClasses || []}>
       {props.children &&
         Children.toArray(props.children).map((child, index) => (
           <SCGridItem
             item
             key={index}
             style={{
-              ...(gridToWidth(child?.props?.gridProps?.gridSize)||{}),
-              paddingLeft: props.spacing? props.spacing * 8 : 8,
-              paddingTop: props.spacing? props.spacing * 8 : 8
-            }}
-          >
+              ...(gridToWidth(child?.props?.gridProps?.gridSize) || {}),
+              paddingLeft: padding,
+              paddingTop: padding,
+            }}>
             {child}
           </SCGridItem>
         ))}
