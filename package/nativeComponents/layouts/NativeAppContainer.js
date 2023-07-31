cd27711083fabdeffa90eaaa3f9b2712e6280a2b
techoneel <contact.techoneel@gmail.com>
2023-07-31 16:39:18 +0530

    + added logged out page container

--- package/nativeComponents/layouts/NativeAppContainer.js
+++ package/nativeComponents/layouts/NativeAppContainer.js
@@ -1,18 +1,23 @@
 import React from "react";
 import NativeBox from "./NativeBox";
-import {useTheme} from "react-native-paper";
+import { useTheme } from "react-native-paper";
 
 export default function NativeAppContainer(props) {
-  const { appBar, leftDrawer, coreClasses, uid } = props;
-  const theme = useTheme()
+  const { appBar, leftDrawer, footer, uid, coreClasses } = props;
+  const theme = useTheme();
   return (
     <>
       {uid && appBar()}
 
-      <NativeBox style={uid? { height: "92%" }: {backgroundColor:theme.colors.background}}>
+      <NativeBox
+        style={
+          uid ? { height: "92%" } : { backgroundColor: theme.colors.background }
+        }
+      >
         {leftDrawer()}
 
         {props.children}
+        {footer()}
       </NativeBox>
     </>
   );
--- package/nativeComponents/layouts/NativePageContainer.js
+++ package/nativeComponents/layouts/NativePageContainer.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import NativeBox from './NativeBox';
-import {ScrollView, Dimensions, KeyboardAvoidingView} from 'react-native';
+import React from "react";
+import NativeBox from "./NativeBox";
+import { ScrollView, Dimensions, KeyboardAvoidingView } from "react-native";
 
 export default function NativePageContainer(props) {
-  const {coreClasses, uid} = props;
+  const { coreClasses, uid } = props;
   /**
    * @todo scroll view is used in page container should be removed
    * when flatlist used but this is causing children in pages being
    * overlapped that's why created a bounding box with fixed height
    * in flat list for data table
    */
-  const windowHeight = Dimensions.get('window').height;
+  const windowHeight = Dimensions.get("window").height;
   const DEFAULT_APP_BAR_HEIGHT = 64;
 
   return (
     <NativeBox
       style={{
         height: uid ? windowHeight - DEFAULT_APP_BAR_HEIGHT : windowHeight,
-        height: uid ? windowHeight - DEFAULT_APP_BAR_HEIGHT : '100%',
-        position: uid ? 'absolute' : 'relative',
+        height: uid ? windowHeight - DEFAULT_APP_BAR_HEIGHT : "100%",
+        position: uid ? "absolute" : "relative",
       }}
-      styleClasses={[coreClasses?.LAYOUT?.PAGE_CONTAINER]}>
-      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      styleClasses={
+        uid
+          ? [coreClasses.LAYOUT.PAGE_CONTAINER]
+          : [coreClasses.LAYOUT.LOGGED_OUT_PAGE_CONTAINER]
+      }
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         {props.children}
       </ScrollView>
     </NativeBox>
