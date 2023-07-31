4bc6c1e05763b03a4f4b86e8d8802200dd29907b
sumanta-m <sumanta.m@vixplor.com>
2023-07-31 13:52:41 +0530

    auth theme change

--- package/nativeComponents/layouts/NativeAppContainer.js
+++ package/nativeComponents/layouts/NativeAppContainer.js
@@ -1,13 +1,15 @@
 import React from "react";
 import NativeBox from "./NativeBox";
+import {useTheme} from "react-native-paper";
 
 export default function NativeAppContainer(props) {
   const { appBar, leftDrawer, coreClasses, uid } = props;
+  const theme = useTheme()
   return (
     <>
       {uid && appBar()}
 
-      <NativeBox style={uid && { height: "92%" }}>
+      <NativeBox style={uid? { height: "92%" }: {backgroundColor:theme.colors.background}}>
         {leftDrawer()}
 
         {props.children}
