4691bc78f597f80f7c908869ae641825353bb88b
sumanta-m <sumanta.m@vixplor.com>
2023-09-08 19:17:34 +0530

    native pop over corretion

--- package/nativeComponents/utils/NativePopover.js
+++ package/nativeComponents/utils/NativePopover.js
@@ -1,15 +1,25 @@
-import React from "react";
-import NativeBox from "../layouts/NativeBox";
-import NativeIcon from "../dataDisplay/NativeIcon";
-import NativeIconButton from "../inputs/NativeIconButton";
+import React, {useEffect, useRef, useState} from 'react';
+import NativeBox from '../layouts/NativeBox';
+import NativeIcon from '../dataDisplay/NativeIcon';
+import NativeIconButton from '../inputs/NativeIconButton';
 
-import { UtilityClasses } from "@wrappid/styles";
+import {UtilityClasses} from '@wrappid/styles';
+import {Animated} from 'react-native';
 
 export default function NativePopover(props) {
-  const { open, onClose, ...restProps } = props;
+  const {open, onClose, ...restProps} = props;
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
-  console.log("open", open);
-  console.log("restPorps", restProps);
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  console.log('open', open);
+  console.log('restPorps', restProps);
 
   return (
     props.open && (
@@ -21,49 +31,48 @@ export default function NativePopover(props) {
         // @todo should be removed if background color opacity support
         // can be given in styleclasses
         style={{
-          backgroundColor: "rgba(0,0,0,0.3)",
+          backgroundColor: 'rgba(0,0,0,0.3)',
           zIndex: 100,
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}>
         <NativeBox
-          // style={{marginTop: 300, marginBottom: 320, marginLeft: 50}}
           style={{
-            position: "absolute",
+            position: 'absolute',
             top: 200,
           }}
           styleClasses={[
             UtilityClasses?.DISPLAY?.FLEX,
             UtilityClasses?.OPACITY?.OPACITY_100,
             UtilityClasses?.WIDTH?.W_75,
-          ]}
-        >
-          <NativeBox
-            style={{ flex: 1, zIndex: 200 }}
-            styleClasses={[
-              UtilityClasses?.BG?.BG_WHITE,
-              UtilityClasses?.PADDING?.PY2,
-            ]}
-          >
+          ]}>
+          <Animated.View style={{opacity: fadeAnim}}>
             <NativeBox
-              style={{
-                marginTop: -25,
-              }}
+              style={{flex: 1, zIndex: 200, borderRadius: 10}}
               styleClasses={[
-                UtilityClasses.DISPLAY?.FLEX,
-                UtilityClasses.ALIGNMENT?.ALIGN_ITEMS_END,
-              ]}
-            >
-              <NativeIconButton onClick={() => onClose && onClose()}>
-                <NativeIcon name="close" type="material-icon">
-                  close
-                </NativeIcon>
-              </NativeIconButton>
+                UtilityClasses?.BG?.BG_WHITE,
+                UtilityClasses?.PADDING?.PY2,
+              ]}>
+              <NativeBox
+                style={{
+                  marginTop: -25,
+                }}
+                styleClasses={[
+                  UtilityClasses.DISPLAY?.FLEX,
+                  UtilityClasses.ALIGNMENT?.ALIGN_ITEMS_END,
+                ]}>
+                <NativeIconButton
+                  // styleClasses={[UtilityClasses?.BG?.BG_WHITE]}
+                  onClick={() => onClose && onClose()}>
+                  <NativeIcon name="close" type="material-icon">
+                    close
+                  </NativeIcon>
+                </NativeIconButton>
+              </NativeBox>
+              {props.children}
             </NativeBox>
-            {props.children}
-          </NativeBox>
+          </Animated.View>
         </NativeBox>
       </NativeBox>
     )
