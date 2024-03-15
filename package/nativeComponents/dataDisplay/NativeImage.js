c4c065b66843e55c6e561ecc4eaba69af88156dd
Swrnv-qc <royswarnava5@gmail.com>
2024-03-15 11:15:32 +0530

    feat: :sparkles: added image ratio
    native image works with height or width props only
    
    Ref: #49

--- package/nativeComponents/dataDisplay/NativeImage.js
+++ package/nativeComponents/dataDisplay/NativeImage.js
@@ -1,12 +1,31 @@
+// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
 import React from "react";
+
+// eslint-disable-next-line import/namespace
+import { Image } from "react-native";
+
 import { SCImage } from "../../styledComponents/dataDisplay/SCImage";
 
 export default function NativeImage(props) {
   const { src, height, width, ...restProps } = props;
+  const autoadjust = (src, height, width) => {
+    Image.getSize(src, (originalHeight, originalWidth) => {
+      const ratio = originalWidth / originalHeight;
+
+      if (!width) {
+        width = height / ratio;
+      } else {
+        height = width * ratio;
+      }
+      return height, width;
+    });
+  };
 
   return (
     <SCImage
-      style={height&&width?{ height, width }:{}}
+      style={
+        height && width ? { height, width } : autoadjust(src, height, width)
+      }
       source={
         src
           ? typeof src === "string" && src.includes("http")
