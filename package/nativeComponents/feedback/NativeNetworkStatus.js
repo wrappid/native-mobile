6dc49e3784e20b1b5bcaa9d9e657fe676e07bd61
EdithMark42 <edithmark42@gmail.com>
2024-03-16 01:50:33 +0530

    feat(global): :sparkles: networkStatus indicator implementation
    networkStatus indicator implementation in native-mobile
    
    ref: #51

--- package/nativeComponents/feedback/NativeNetworkStatus.js
+++ package/nativeComponents/feedback/NativeNetworkStatus.js
@@ -1,4 +1,41 @@
+// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
+import React, { useState, useEffect } from "react";
 
-export default function NativeNetworkStatus() {
-  return (<></>);
-}
+import { addEventListener } from "@react-native-community/netinfo";
+import Snackbar from "react-native-snackbar";
+
+const NativeNetworkStatus = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [isOffline, setIsOffline] = useState(false);
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = addEventListener(state => {
+      setIsOffline(!state.isConnected);
+      if (!state.isConnected) {
+        setSnackbarVisible(true);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (snackbarVisible) {
+      isOffline ? 
+        Snackbar.show({
+          duration: Snackbar.LENGTH_INDEFINITE,
+          text    : "🔴 You are offline!",
+        }) :
+        Snackbar.show({
+          duration: Snackbar.LENGTH_LONG,
+          text    : "🟢 You are online!",
+        });
+    }});
+
+  return null; // This component doesn't render anything in the UI
+};
+
+export default NativeNetworkStatus;
