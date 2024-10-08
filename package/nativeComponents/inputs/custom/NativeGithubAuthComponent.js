5dd34300d0d5639faa8b44352531df795485d0ad
priyanshu2k3 <priyanshu9836@gmail.com>
2024-10-08 14:09:05 +0530

    feat: :sparkles: adds the github auth component in the native mobile
    adds the github auth component in the native mobile
    
    ref: #89

---
+++ package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line unused-imports/no-unused-imports
+import React from "react";
+export default function NativeGithubAuthComponent(props) {
+  const { children } = props;
+
+  return (<div>{children}</div>);
+}
+  
