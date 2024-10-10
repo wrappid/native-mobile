067b1978e30c6c40004a68054ea387afbcbc4482
priyanshu2k3 <priyanshu9836@gmail.com>
2024-10-10 12:35:49 +0530

    fix(core): :sparkles: linting  fix
    linting  fix
    
    ref: #89

--- package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
+++ package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line unused-imports/no-unused-imports
+// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
 import React from "react";
 export default function NativeGithubAuthComponent(props) {
   const { children } = props;
