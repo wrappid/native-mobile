ade602e3c2ab1d9da85c28dd7ee1f466b4e6c7aa
anantakumarghosh <antaghosh@gmail.com>
2024-10-19 18:17:01 +0530

    fix(core): :bug: github button
    separate code required for mobile, use of sdks suggested
    
    ref: #96

--- package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
+++ package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
@@ -1,10 +1,59 @@
-// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import NativeBox from "../../layouts/NativeBox";
-export default function NativeGithubAuthComponent(props) {
-  const { children } = props;
+// eslint-disable-next-line import/no-unresolved
+import { WrappidDataContext } from "@wrappid/styles";
+// eslint-disable-next-line import/namespace
+import { Linking, TouchableOpacity } from "react-native";
 
-  return (<NativeBox>{children}</NativeBox>);
+import NativeIcon from "../../dataDisplay/NativeIcon";
+
+// eslint-disable-next-line etc/no-commented-out-code
+// const GITHUB_ICON_URL = "https://www.svgrepo.com/show/512317/github-142.svg";
+
+export default function NativeGithubAuthComponent({ onAuthCodeReceived, ...props }) {
+  const { config } = React.useContext(WrappidDataContext);
+  const [github_code, setGithub_code] = useState("");
+
+  useEffect(() => {
+    const handleDeepLink = (event) => {
+      const { url } = event;
+      const code = url.match(/code=([^&]*)/);
+
+      if (code && code[1]) {
+        setGithub_code(code[1]);
+      }
+    };
+
+    Linking.addEventListener("url", handleDeepLink);
+    return () => {
+      Linking.removeEventListener("url", handleDeepLink);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (github_code === "") return;
+    onAuthCodeReceived(github_code);
+    setGithub_code("");
+  }, [github_code, onAuthCodeReceived]);
+
+  const handleAuthoriseGithub = async () => {
+    const clientId = config.wrappid.socialLogin.github.clientId;
+    const redirectUri = config.wrappid.socialLogin.github.redirectUri;
+    const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo,discussions,read:user,user:email`;
+
+    const supported = await Linking.canOpenURL(authUrl);
+
+    if (supported) {
+      await Linking.openURL(authUrl);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error("Don't know how to open URI: " + authUrl);
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={handleAuthoriseGithub} {...props}>
+      <NativeIcon type="far" name="fa-brands fa-github" childrenFlag={true} />
+    </TouchableOpacity>
+  );
 }
-  
